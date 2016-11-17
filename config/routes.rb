8c227087f744b5aec9bc903cf5006bcafed32131
Alessandra <alessandra.m.vasquez@gmail.com>
2016-11-17 11:40:18 -0500

    spike out signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resource :users
+
+
+  # get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
